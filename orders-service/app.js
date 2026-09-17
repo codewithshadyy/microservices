5d822950334c93998eee669560d3cd58bbf60dc8
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-09-17 20:42:07 +0300

    ORE4DRS

---
+++ orders-service/app.js
@@ -0,0 +1,13 @@
+
+const express = require("express")
+const app = express()
+const orders = require("./routes/orders")
+PORT=3002
+
+app.use(express.json())
+
+app.use("/orders", orders)
+
+app.listen(PORT, () =>{
+    console.log(`http://localhost:${PORT}`)
+})
---
+++ orders-service/routes/orders.js
@@ -0,0 +1,14 @@
+
+const express = require("express")
+const app = express.Router()
+
+
+app.get("/orders", async (req,res) => {
+
+   return  res.status(200).json({
+        message:"Fetching orders"
+    })
+    
+})
+
+module.exports = app
